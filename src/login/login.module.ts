c8d6e2373e096285ef0b8546295a4f41559119ab
nir@500tech.com <nir@500tech.com>
2016-11-08 16:04:23 +0200

    r

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -7,7 +7,6 @@ import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [LoginComponent, InfoComponent, FormComponent],
-
     imports: [BrowserModule],
     exports: [LoginComponent]
 })
